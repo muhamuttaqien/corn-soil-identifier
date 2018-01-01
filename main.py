d8c47198eee6751adce152252400ee2bf6e6b06c
Muh. Angga Muttaqien <angga.muttaqien@ruma.co.id>
2018-01-01 15:01:32 +0700

    Add histogram

--- main.py
+++ main.py
@@ -13,6 +13,50 @@ from matplotlib import pyplot as plt
 from preprocessing import convert_to_gray, threshold, remove_noise, contrast, convolve, equalize_histogram
 
 
+
+def analyze_histogram(img):
+    """
+    find and analyze histogram
+    """
+    # convert first
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+    # analyze it
+    hist = cv2.calcHist([img],[0],None,[256],[0,256])
+
+    return hist
+
+
+def plot_histogram(img):
+    """
+    plot and analyze histogram for gray image
+    """
+    hist = cv2.calcHist([img],[0],None,[256],[0,256])
+
+    plt.hist(hist, facecolor='green')
+    plt.title('Histogram'), plt.xlabel("Scale"), plt.ylabel("Quantity")
+    plt.grid(True)
+
+    plt.show()
+
+
+def plot_color_histogram(img):
+    """
+    plot and analyze histogram for color image
+    """
+    color = ('b', 'g', 'r')
+    for i, col in enumerate(color):
+        hist = cv2.calcHist([img],[i],None,[256],[0,256])
+        
+        plt.hist(hist, color = col)
+        plt.xlim([0,256])
+    
+    plt.title('Color Histogram'), plt.xlabel("Scale"), plt.ylabel("Quantity")
+    plt.grid(True)
+
+    plt.show()
+
+
 def load_image(path, resize):
     """
     load soil image and resize proportionally 
@@ -41,10 +85,6 @@ def get_processed_image(img):
     return ret, result
 
 
-def get_region_of_interest():
-    pass
-
-
 def main():
     # setting the hyper parameters
     parser = argparse.ArgumentParser()
@@ -56,6 +96,7 @@ def main():
     resize = args.resize
 
     img = load_image(path, resize)
+    plot_color_histogram(img)
     ret, result = get_processed_image(img)
 
     cv2.imshow('Original Image', img)

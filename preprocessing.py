f0b11ba0521bfade768509e9bdc084c49f6dbd59
Muh. Angga Muttaqien <angga.muttaqien@ruma.co.id>
2018-01-02 06:38:56 +0700

    add other morph transformation

--- preprocessing.py
+++ preprocessing.py
@@ -3,27 +3,23 @@ import numpy as np
 
 
 def convert_to_gray(img):
-
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     return img
 
 
 def threshold(img, val):
-
     ret, img = cv2.threshold(img,val,255,cv2.THRESH_BINARY)
     return ret, img
 
 
 def remove_noise(img):
-
     img = cv2.medianBlur(img,5)
     return img
 
 
 def contrast(img):
-
     kernel = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
-    img = cv2.filter2D(img, -1, kernel)
+    img = cv2.filter2D(img,-1,kernel)
     return img
 
 
@@ -32,13 +28,30 @@ def convolve(img):
 
 
 def equalize_histogram(img):
-   
     img = cv2.equalizeHist(img)
     # res = np.hstack((img,equ) #stacking images side-by-side
     return img
 
 
-def close_morph(img):
-    kernel = np.ones((10,10),np.uint8)
-    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+def erose_morph(img, size, iter):
+    kernel = np.ones((size,size),np.uint8)
+    img = cv2.erode(img,kernel,iterations = iter)
+    return img
+
+
+def dilate_morph(img, size, iter):
+    kernel = np.ones((size,size),np.uint8)
+    img = cv2.dilate(img,kernel,iterations = iter)
+    return img
+
+
+def open_morph(img, size):
+    kernel = np.ones((size,size),np.uint8)
+    img = cv2.morphologyEx(img,cv2.MORPH_OPEN, kernel)
+    return img
+
+
+def close_morph(img, size):
+    kernel = np.ones((size,size),np.uint8)
+    img = cv2.morphologyEx(img,cv2.MORPH_CLOSE, kernel)
     return img
